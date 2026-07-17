871b138cb1c52e6014d9d92ccffb5589fc44f01e
Bhargo Raidas <raidasp522@gmail.com>
2026-07-17 20:41:12 +0530

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,30 @@
+
+name = input("tumhara naam kya hai bhai?").strip().lower()
+category = input("category select karo  " \
+"joke " \
+"tumhara age analysis batau (adult ya student)" \
+"Favorite subject ke hisab se suggestion de (match-case use karke)" \
+"exit(leave)")
+match category:
+    case joke if category == joke:
+        print("life badi asaan hai na?")
+    case analysis if category ==  analysis:
+        print("ok")
+        age = int(input("tumhara age?"))
+        
+        if age >= 18:
+            print("tum abhi adult ho")
+        else:
+            print("tm abhi student ho kyoki tum abhi adult nahi ho")
+        print("Aur tumhara naam hai" + name)
+    case suggestion if category == suggestion:
+        subject = input("your faivorite subject?").strip().lower()
+
+        if subject == "maths":
+            print("acha scope hai")
+        elif subject == "science":
+            print("good scope, better future")
+        else:
+            print("dusre  subject me bhi dhyan do")
+    case exit if category == exit:
+            print("bye")
